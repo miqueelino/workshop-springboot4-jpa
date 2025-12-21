c1ed04060a37fee3b1371cf92519416d9190dae1
miqueelino <joao.jnunes@mercadolivre.com>
2025-12-21 16:15:09 -0300

    Project created

---
+++ demo/src/main/java/com/projetojp/demo/ProjetojpApplication.java
@@ -0,0 +1,13 @@
+package com.projetojp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojpApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/projetojp/demo/ProjetojpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetojp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
