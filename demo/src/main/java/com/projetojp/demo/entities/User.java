3ac0f98af23759da6cf1ce1676b930355803bcbd
miqueelino <joao.jnunes@mercadolivre.com>
2025-12-23 17:06:25 -0300

    Order, Instant, ISO 8601 part 1

--- demo/src/main/java/com/projetojp/demo/entities/User.java
+++ demo/src/main/java/com/projetojp/demo/entities/User.java
@@ -1,12 +1,15 @@
 package com.projetojp.demo.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +28,10 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order>orders = new ArrayList<>();
 	
 
 	public User() {
@@ -91,6 +98,10 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -109,6 +120,9 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
+	
 	
 	
 }
