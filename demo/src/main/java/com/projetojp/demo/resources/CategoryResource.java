7ab31c8b5517e3e9351863d2564d8cbe6d3cd0ce
miqueelino <joao.jnunes@mercadolivre.com>
2025-12-24 16:05:42 -0300

    Category

---
+++ demo/src/main/java/com/projetojp/demo/resources/CategoryResource.java
@@ -0,0 +1,43 @@
+package com.projetojp.demo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetojp.demo.entities.Category;
+import com.projetojp.demo.entities.User;
+import com.projetojp.demo.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	
+	@Autowired
+	private CategoryService service;
+ 
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		
+		List<Category> list = service.findAll();
+				
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+	
+	
+}
