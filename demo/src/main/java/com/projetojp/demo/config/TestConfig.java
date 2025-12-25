10fe9060c2f3a144742d488d1c3cae4bc1cac318
miqueelino <joao.jnunes@mercadolivre.com>
2025-12-25 15:09:49 -0300

    Payment, one-to-one association

--- demo/src/main/java/com/projetojp/demo/config/TestConfig.java
+++ demo/src/main/java/com/projetojp/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetojp.demo.entities.Category;
 import com.projetojp.demo.entities.Order;
 import com.projetojp.demo.entities.OrderItem;
+import com.projetojp.demo.entities.Payment;
 import com.projetojp.demo.entities.Product;
 import com.projetojp.demo.entities.User;
 import com.projetojp.demo.entities.enums.OrderStatus;
@@ -101,6 +102,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 
