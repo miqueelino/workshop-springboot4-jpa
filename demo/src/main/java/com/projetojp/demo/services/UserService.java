73ea7c73975d19cb06856f3755579d18788d948d
miqueelino <joao.jnunes@mercadolivre.com>
2025-12-26 14:17:23 -0300

    User update

--- demo/src/main/java/com/projetojp/demo/services/UserService.java
+++ demo/src/main/java/com/projetojp/demo/services/UserService.java
@@ -1,6 +1,5 @@
 package com.projetojp.demo.services;
 
-
 import java.util.List;
 import java.util.Optional;
 
@@ -15,27 +14,36 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
-    public User findById(Long id) {
-		
+
+	public User findById(Long id) {
+
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
-		
+
 	}
-    
-    
-    public User insert(User obj) {
+
+	public User insert(User obj) {
 		return repository.save(obj);
 	}
-    
-    
-    public void delete(Long id) {
-    	repository.deleteById(id);
-    }
-    
-	
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+
 }
